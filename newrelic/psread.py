c0ab6d7157dfaf5637b9026a47af72c9544a5580
Michael Osowski <mosowski@newrelic.com>
2016-06-15 11:45:47 -0400

    adding pagefaults

--- newrelic/psread.py
+++ newrelic/psread.py
@@ -37,11 +37,22 @@ def getps(inRegex,inlogging,shostname):
         else:
             testexe = str(pinfo['cmdline'])
             if regex.search(testexe) is not None:
+                
+                
                 singleresponse={}
                 singleresponse['eventType'] = "SystemStat"
                 singleresponse['hostname'] = pshost
                 singleresponse['cpu'] = pinfo['cpu_percent']
-                singleresponse['pagefults'] = pinfo['memory_info']
+                singleresponse['rss'] = pinfo['memory_info'][0]
+                singleresponse['vms'] = pinfo['memory_info'][1]
+                singleresponse['shared'] = pinfo['memory_info'][2]
+                singleresponse['mtext'] = pinfo['memory_info'][3]
+                try:
+                    singleresponse['lib'] = pinfo['memory_info'][4]
+                    singleresponse['data'] = pinfo['memory_info'][5]
+                    singleresponse['dirty'] = pinfo['memory_info'][6]
+                except Exception:
+                    pass    
                 singleresponse['pid'] = pinfo['pid']
                 singleresponse['memory'] = pinfo['memory_percent']
                 singleresponse['commandline'] = pinfo['cmdline']
